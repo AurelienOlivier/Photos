b6d87bde1d5f7e6e8d447e6f72d1912427f1aead
Aurelien Olivier <olivier.aurelien@gmail.com>
2012-08-24 22:40:05 +0200

    User management modification
    The username is "admin" with password "foo"

--- web/index.php
+++ web/index.php
@@ -1,8 +1,9 @@
 <?php
+use Silex\Application\path;
+
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../lib/pillow/bootstrap.php';
 
-use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -14,9 +15,29 @@ $app = new Silex\Application();
 $app['debug'] = true;
 
 // Register Twig
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\SecurityServiceProvider());
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
 	'twig.path' => __DIR__ . '/../templates',
 ));
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+	'monolog.logfile' => __DIR__.'/../logs/development.log',
+));
+
+// Define security settings
+$app['security.firewalls'] = array(
+	'admin' => array(
+		'pattern' => '^/',
+		'anonymous' => array(),
+		'form' => array('login_path' => '/login', 'check_path' => '/admin/login_check'),
+		'logout' => array('logout_path' => '/logout', 'target_url' => '/'),
+		'users' => array(
+			// raw password is foo
+			'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
+		),
+	),
+);
 
 // CouchDB connection
 phpillowConnection::createInstance('localhost', 5984, 'root', 'root');
@@ -29,9 +50,10 @@ $app->get('/', function (Silex\Application $app) {
 	$photos = PhotoView::entries();
 	
 	return $app['twig']->render('welcome.twig', array(
-		'photos' => $photos->rows
+		'photos' => $photos->rows,
+		'app' => $app,
 	));
-});
+})->bind("home");
 
 // display a photo
 $app->get('/photo/{id}', function (Silex\Application $app, $id) {
@@ -41,10 +63,21 @@ $app->get('/photo/{id}', function (Silex\Application $app, $id) {
 	return $app['twig']->render('photo.twig', array('photo' => $photo));
 });
 
+// login form
+$app->get('/login', function(Request $request) use ($app) {
+	return $app['twig']->render('login.twig', array(
+		'error'         => $app['security.last_error']($request),
+		'last_username' => $app['session']->get('_security.last_username'),
+		'app' => $app,
+	));
+})->bind("login");
+
 // add form
 $app->get('/add', function (Silex\Application $app) {
-	return $app['twig']->render('add.twig');
-});
+	return $app['twig']->render('admin/add.twig', array(
+		'app' => $app,
+	));
+})->bind("add");
 
 $app->post('/create', function (Request $request) use ($app) {
 	$title = $request->get("title");
@@ -69,7 +102,12 @@ $app->post('/create', function (Request $request) use ($app) {
 	} else {
 		throw new Exception("Missing parameters in upload form");
 	}
-});
+})->bind("create");
+
+// user management
+$app->get("/profile", function() {
+	
+})->bind("profile");
 
 // image handler
 $app->get('/photoview/{id}.{ext}', function (Silex\Application $app, $id, $ext) {
